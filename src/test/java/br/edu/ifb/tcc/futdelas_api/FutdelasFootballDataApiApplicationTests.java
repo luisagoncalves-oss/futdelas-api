37465f1c4b456b803ae9b209ab3712991bf603a7
luisagoncalves <luisaolg20@gmail.com>
2025-09-01 23:29:15 -0300

    chore: add project config

---
+++ src/test/java/br/edu/ifb/tcc/futdelas_api/FutdelasFootballDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifb.tcc.futdelas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutdelasFootballDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
