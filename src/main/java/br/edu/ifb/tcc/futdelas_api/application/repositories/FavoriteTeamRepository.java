4d72545364e267645c4673dbebd9f277f087352a
luisagoncalves <luisaolg20@gmail.com>
2025-09-14 15:28:02 -0300

    fix change type

--- src/main/java/br/edu/ifb/tcc/futdelas_api/application/repositories/FavoriteTeamRepository.java
+++ src/main/java/br/edu/ifb/tcc/futdelas_api/application/repositories/FavoriteTeamRepository.java
@@ -1,7 +1,6 @@
 package br.edu.ifb.tcc.futdelas_api.application.repositories;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,5 +11,5 @@ import br.edu.ifb.tcc.futdelas_api.application.model.FavoriteTeam;
 public interface FavoriteTeamRepository extends JpaRepository<FavoriteTeam, Long> {
     boolean existsByAnonymousUserId(String anonymousUserId);
     Optional<FavoriteTeam> findByAnonymousUserId(String anonymousUserId);
-    void deleteById(UUID id);
+    void deleteById(Long id);
 }
