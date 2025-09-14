ab2ed5252098f7ca395cb86a9e55084f805b8892
luisagoncalves <luisaolg20@gmail.com>
2025-09-14 15:06:43 -0300

    fix: change type id

--- src/main/java/br/edu/ifb/tcc/futdelas_api/application/repositories/TeamRepository.java
+++ src/main/java/br/edu/ifb/tcc/futdelas_api/application/repositories/TeamRepository.java
@@ -1,6 +1,7 @@
 package br.edu.ifb.tcc.futdelas_api.application.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 import br.edu.ifb.tcc.futdelas_api.application.model.Team;
 
 @Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
+public interface TeamRepository extends JpaRepository<Team, UUID> {
     Optional<Team> findByName(String name);
 }
