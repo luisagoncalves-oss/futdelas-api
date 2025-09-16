4ab3c4a4adbb6296238b253362bfc31867bb70ba
luisagoncalves <luisaolg20@gmail.com>
2025-09-16 19:28:58 -0300

    change config

--- src/main/resources/migration/V1__create_favorite_teams_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE favorite_teams (
-    favorite_team_id BIGSERIAL PRIMARY KEY,
-    anonymous_user_id VARCHAR(255) NOT NULL UNIQUE,
-    team_id UUID NOT NULL,
-    is_favorite BOOLEAN DEFAULT true,
-    created_at TIMESTAMP
-);
