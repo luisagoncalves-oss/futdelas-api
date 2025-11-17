12473f492fa93d0cf68811e30517eda3e1c8a9c4
luisagoncalves <luisaolg20@gmail.com>
2025-11-16 21:42:25 -0300

    fix type

--- src/main/java/br/edu/ifb/tcc/futdelas_api/model/UniqueTournament.java
+++ src/main/java/br/edu/ifb/tcc/futdelas_api/model/UniqueTournament.java
@@ -1,24 +1,13 @@
 package br.edu.ifb.tcc.futdelas_api.model;
 
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 @JsonIgnoreProperties(ignoreUnknown = true) 
 public class UniqueTournament {
     private String name;
     private TitleHolder titleHolder;
-    
-    @JsonProperty("startDateTimestamp")
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime startDateTimestamp;
-
-    @JsonProperty("endDateTimestamp")
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    private LocalDateTime endDateTimestamp;
+    private Long startDateTimestamp;
+    private Long endDateTimestamp;
 
     public String getName() {
         return name;
@@ -32,16 +21,16 @@ public class UniqueTournament {
     public void setTitleHolder(TitleHolder titleHolder) {
         this.titleHolder = titleHolder;
     }
-    public LocalDateTime getStartDateTimestamp() {
+    public Long getStartDateTimestamp() {
         return startDateTimestamp;
     }
-    public void setStartDateTimestamp(LocalDateTime startDateTimestamp) {
+    public void setStartDateTimestamp(Long startDateTimestamp) {
         this.startDateTimestamp = startDateTimestamp;
     }
-    public LocalDateTime getEndDateTimestamp() {
+    public Long getEndDateTimestamp() {
         return endDateTimestamp;
     }
-    public void setEndDateTimestamp(LocalDateTime endDateTimestamp) {
+    public void setEndDateTimestamp(Long endDateTimestamp) {
         this.endDateTimestamp = endDateTimestamp;
     }
 }
